262fbbc24936d43aed08a316960bfa9948cb9ca8
pavel-pl-18 <pavel.pl18@yahoo.com>
2019-01-27 00:10:38 +0100

    JUnit dependencies + test

--- round-1/src/test/java/com/hexad/sales/round1/Round1ApplicationTests.java
+++ round-1/src/test/java/com/hexad/sales/round1/Round1ApplicationTests.java
@@ -1,16 +1,19 @@
 package com.hexad.sales.round1;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class Round1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertNotNull("works");
 	}
 
 }
