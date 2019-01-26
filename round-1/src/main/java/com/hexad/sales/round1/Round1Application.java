a4ac578990e239cd72b2be4d078c8d8eced1509c
pavel-pl-18 <pavel.pl18@yahoo.com>
2019-01-26 16:17:48 +0100

    init

---
+++ round-1/src/main/java/com/hexad/sales/round1/Round1Application.java
@@ -0,0 +1,14 @@
+package com.hexad.sales.round1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Round1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Round1Application.class, args);
+	}
+
+}
+
---
+++ round-1/src/test/java/com/hexad/sales/round1/Round1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hexad.sales.round1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Round1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
