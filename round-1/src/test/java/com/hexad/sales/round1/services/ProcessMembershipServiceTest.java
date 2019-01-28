fa3b2d8baa0b786f2974b7b4d1e1d4d98fdae60d
pavel-pl-18 <pavel.pl18@yahoo.com>
2019-01-28 15:07:13 +0100

    ProcessMembershipServiceTest.processMembership_exceptions

--- round-1/src/test/java/com/hexad/sales/round1/services/ProcessMembershipServiceTest.java
+++ round-1/src/test/java/com/hexad/sales/round1/services/ProcessMembershipServiceTest.java
@@ -2,9 +2,16 @@ package com.hexad.sales.round1.services;
 
 import com.hexad.sales.round1.entities.master.Firm;
 import com.hexad.sales.round1.enums.MembershipStatus;
+import com.hexad.sales.round1.exceptions.ProcessMembershipException;
 import com.hexad.sales.round1.utils.DataStubs;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static com.hexad.sales.round1.exceptions.ProcessMembershipException.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessMembershipServiceTest {
@@ -19,6 +26,26 @@ class ProcessMembershipServiceTest {
     service.processMembership(firm);
     // then
     assertEquals(firm.getMembershipStatus(), MembershipStatus.ACTIVE);
+  }
+
+  @ParameterizedTest
+  @MethodSource("processMembership_exceptions_data")
+  void processMembership_exceptions(MembershipStatus membershipStatus,
+                                    Cause cause) {
+    // given
+    Firm firm = DataStubs.getFirms1();
+    firm.setMembershipStatus(membershipStatus);
+    // when then
+    Throwable exception = assertThrows(ProcessMembershipException.class, () -> {
+      service.processMembership(firm);
+    });
+    assertEquals(cause.name(), exception.getMessage());
+  }
+
+  private static Stream<Arguments> processMembership_exceptions_data() {
+    return Stream.of(
+        Arguments.of(MembershipStatus.ACTIVE, Cause.CLIENT_IS_ALREADY_MEMBER),
+        Arguments.of(MembershipStatus.UPGRATED, Cause.CANNOT_ACTIVATE_MEMBERSHIP_CLIENT_IS_UPGRATED_MEMBER));
   }
 
 
